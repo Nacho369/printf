d7246476d3e0724cd3caea94c8965f8881b4b57a
mb <michaelsenapon@gmail.com>
2023-06-29 19:11:25 +0100

    handling %p conversion specifier

--- print_formats_two.c
+++ print_formats_two.c
@@ -150,23 +150,16 @@ int p_Str(va_list args_param)
 int p_address(va_list args_param)
 {
 	int len = 0, hex = 16;
-	int num;
-	unsigned long int digit;
-	unsigned int n  = va_arg(args_param, int);
+	void *ptr;
+	ptr = va_arg(args_param, void *);
+
+	if (ptr == NULL)
+	{
+		len = print_str("(null)");
+		return (len);
+	}
 	
-	num = n;
-
-	if (num < 0)
-	{
-		digit = num * -1;
-		digit = ~digit + 1;
-	}
-	else
-	{
-		digit = n;
-	}
-
 	len += print_str("0x");
-	len += print_hex(digit, hex, 1);
+	len += print_hex(*ptr, hex, 1);
 	return (len);
 }
